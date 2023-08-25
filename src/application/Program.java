d07979ac044ec0cfdbf42d6487e7cb7c88e679aa
Leonardo Laranjeira <leonardolaranjeiradev@gmail.com>
2023-08-25 06:55:26 -0300

    Try-whit-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,14 +8,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		String path = "d:\\trabalhandocomarquivos\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
+		String path = "D:\\Trabalhando com arquivos\\in.txt";
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 			
 			while (line != null) {
@@ -26,20 +21,7 @@ public class Program {
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-		}
+		
 
 	}
 
