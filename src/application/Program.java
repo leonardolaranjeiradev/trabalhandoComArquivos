108f9f140ca940f23f426338fc5eb811c263e8f7
Leonardo Laranjeira <leonardolaranjeiradev@gmail.com>
2023-08-25 07:26:25 -0300

    Informaçoes do caminho do arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,25 +9,14 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Insira um caminho de pasta: ");
+		System.out.println("Insira um caminho de arquivo: ");
 		String strPath = sc.nextLine();
 		
 		File path = new File(strPath);
 		
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS:");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-		
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILES:");
-		for (File file : files) {
-			System.out.println(file);
-		}
-		
-		boolean success = new File(strPath + "\\subdir").mkdir();
-		System.out.println("Diretório criado com sucesso: " + success);		
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
 		
 		
 		sc.close();
